91040066ef9358dbd67e6e67aed4fe1c8fb772ec
Denis Wilton de Paula Azevedo <denis.azevedo-ext@ab-inbev.com>
2024-10-28 20:44:43 -0300

    Add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 const container = document.getElementById("app");
