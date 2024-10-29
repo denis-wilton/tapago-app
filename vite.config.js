d6195d584811f3c03771fdf8508d94c6182e55b0
Denis Wilton de Paula Azevedo <denis.azevedo-ext@ab-inbev.com>
2024-10-29 02:16:59 -0300

    Teste 007

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
+  base: "./",
   plugins: [react(), svgr()],
   build: {
     outDir: "www", // Onde o Vite vai gerar os arquivos
