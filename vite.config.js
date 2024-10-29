3a964e020cfadc11e98eb9664bfd0dd6ab6cef0e
Denis Wilton de Paula Azevedo <denis.azevedo-ext@ab-inbev.com>
2024-10-29 02:35:43 -0300

    Teste singlefile

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  base: "./",
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), viteSingleFile()],
   build: {
     outDir: "www", // Onde o Vite vai gerar os arquivos
     emptyOutDir: true,
