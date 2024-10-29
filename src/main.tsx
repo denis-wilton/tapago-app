d0a7c0252ed0bf6ab7f9e00e6d4c6476a816d64f
Denis Wilton de Paula Azevedo <denis.azevedo-ext@ab-inbev.com>
2024-10-29 01:25:03 -0300

    Teste 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import { AuthProvider } from "./context/Auth.context";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+
+function RedirectToLogin() {
+  return <Navigate to="/login" />;
+}
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
+          <Route path="/index.html" element={<RedirectToLogin />} />
           <Route path="/" element={<ProtectedRoute />}>
             <Route index element={<Home />} />
             <Route path="cliente" element={<>Oi, eu sou a rota Cliente</>} />
